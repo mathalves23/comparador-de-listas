6fd92bcec34e1e6df5e59aec6b2a2fe98c0e1c87
mathalves23 <matheus.aalves@hotmail.com>
2018-06-19 11:39:38 -0300

    Adicionando o comparador e 2 listas

---
+++ comparador.py
@@ -0,0 +1,28 @@
+import csv
+
+lista1 = []
+lista2 = []
+
+with open('lista1.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        lista1.append(row[0])
+
+with open('lista2.csv', 'r') as csvfile:
+    spamreader2 = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader2:
+        lista2.append(row[0])
+
+
+iguais = []
+diferentes = []
+
+for element in lista2:
+    if element in lista1:
+        iguais.append(element)
+    else:
+        diferentes.append(element)
+
+
+print('Números presentes nas duas listas:', iguais)
+print('Números presentes apenas na lista comparada:', diferentes)
